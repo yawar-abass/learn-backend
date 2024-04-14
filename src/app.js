a406e422e411f477a1bc20cf8ee5be4b8ffc43bb
yawar-abass <bhatsharik6146@gmail.com>
2024-04-14 18:31:08 +0530

    utils- HOF, Error class

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
-// const express = require("express");
-// require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-// const app = express();
+const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("hello there");
-// });
+//cors setting
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-// app.get("/login", (req, res) => {
-//   res.send("Login screen");
-// });
+//to accept data like json url params , and static files
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-// app.listen(process.env.PORT, () => {
-//   console.log("server is listening " + process.env.PORT);
-// });
+// parsing the browser cookies
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+// .env file config
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+// Db connection
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: $${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed !!!", err);
+  });
