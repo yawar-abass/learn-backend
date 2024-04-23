5d302609a4999c906fa8c9e84217f0d268060aae
yawar-abass <bhatsharik6146@gmail.com>
2024-04-23 18:03:50 +0530

    Refactor user.routes.js to add user account management functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { varifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,18 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(varifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(varifyJWT, changeCurrentPassword);
+router.route("/current-user").get(varifyJWT, getCurrentUser);
+router.route("/update-account").patch(varifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(varifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(varifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(varifyJWT, getUserChannelProfile);
+router.route("/history").get(varifyJWT, getWatchHistory);
 
 export default router;
